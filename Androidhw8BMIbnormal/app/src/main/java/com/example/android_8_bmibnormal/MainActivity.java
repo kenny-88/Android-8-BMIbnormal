d230bb4e741ab880ad9436e367112fb06a5b6a42
kenny-88 <71814199+kenny-88@users.noreply.github.com>
2020-10-27 21:06:21 +0800

    Android-8-BMIbnormal

---
+++ Androidhw8BMIbnormal/app/src/main/java/com/example/android_8_bmibnormal/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android_num8_bmiabnormal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void bt1(View v){
+        EditText tv1 = (EditText)findViewById(R.id.ed1);
+        EditText tv2 = (EditText)findViewById(R.id.ed2);
+        Intent intent = new Intent(this, MainActivity2.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("Height", tv1.getText().toString());
+        bundle.putString("Weight", tv2.getText().toString());
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+}
---
+++ Androidhw8BMIbnormal/app/src/main/java/com/example/android_8_bmibnormal/MainActivity2.java
@@ -0,0 +1,45 @@
+package com.example.android_num8_bmiabnormal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        tv4();
+    }
+    public void bt2(View v){
+        finish();
+    }
+    private void tv4(){
+        double height;
+        double weight;
+        double bmi ;
+        Bundle bundle = this.getIntent().getExtras();
+        if (bundle != null){
+            DecimalFormat nf = new DecimalFormat("0.00");
+            height = Double.parseDouble(bundle.getString("Height")) / 100;
+            weight = Double.parseDouble(bundle.getString("Weight"));
+            bmi = weight / (height * height);
+            TextView bmitit = (TextView)findViewById(R.id.tv3);
+            bmitit.setText(nf.format(bmi));
+            TextView abnormal = (TextView)findViewById(R.id.ed4);
+            if (bmi < 18.5){
+                abnormal.setText("異常(體重過輕)");
+            }
+            else if (bmi > 24.5){
+                abnormal.setText("異常(體重過重)");
+            }
+            else
+                abnormal.setText("正常(體重正常)");
+        }
+    }
+}
